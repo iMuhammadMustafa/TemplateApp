d8d7d9517ebfedae2cf979df9d656e2b6b49ad55
iMuhammadMustafa <imohamedmostafa95@gmail.com>
2022-09-04 17:12:10 +0200

    Update webpack.config.js

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,11 +1,11 @@
-const path = require("path");
+// const path = require("path");
 
-module.exports = {
-  resolve: {
-    extensions: [".tsx", ".jsx", ".ts", ".js"],
-    alias: {
-      //"@": path.resolve(__dirname, "src/"),
-      "@features/*": path.resolve(__dirname, "/features/*"),
-    },
-  },
-};
+// module.exports = {
+//   resolve: {
+//     extensions: [".tsx", ".jsx", ".ts", ".js"],
+//     alias: {
+//       //"@": path.resolve(__dirname, "src/"),
+//       "@features": path.resolve(__dirname, "/features/*"),
+//     },
+//   },
+// };
